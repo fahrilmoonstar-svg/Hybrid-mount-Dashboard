b536dfb765dd2c78b697e968561201a2a076e3d3
fahrilmoonstar-svg <fahrilmoonstar@gmail.com>
2026-08-26 17:05:27 +0800

    Add files via upload

---
+++ webui/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solid()],
+  server: { port: 3000, open: true },
+  build: { outDir: 'dist', emptyOutDir: true }
+});
